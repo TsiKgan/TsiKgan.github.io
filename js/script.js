da0d04ea7df9c056fbaaa794d363e42999c61db8
TsiKgan <70378626+TsiKgan@users.noreply.github.com>
2022-06-23 15:00:42 +0800

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -45,10 +45,40 @@ document.addEventListener("click", (e) =>{
 })
 
 
-/*---- theme light dar -----  */
+/*---- theme light dark -----  */
 function themeLightDark(){
+    const switcherBtn = document.querySelector(".js-switcher-btn");
+    const icon = switcherBtn.querySelector("i");
 
-    const switcherBtn = document.querySelector(".js-swticher-btn");
-    const icon = switcherBtn.querySelector("img")
+    function changeIcon(){
+        if(document.body.classList.contains("dark")){
+            icon.classList.remove("fa-moon-o");
+            icon.classList.add("fa-sun-o");
+        }
+        else{
+            icon.classList.remove("fa-sun-o");
+            icon.classList.add("fa-moon-o");
+        }   
+    }
+switcherBtn.addEventListener("click", ()=>{
+    document.body.classList.toggle("dark");
+    changeIcon()
+    if(document.body.classList.contains("dark")){
+        localStorage.setItem("theme","dark");
+    }
+    else{
+        localStorage.setItem("theme","light");
+    }
+})
+    // check for saved user preference, if any, on load of the website
+if(localStorage.getItem("theme")!== null){
+    if(localStorage.getItem("theme")== "light"){
+        document.body.classList.remove("dark");
+    }
+    else{
+        document.body.classList.add("dark");
+    }
+}
+    changeIcon();
 }
 themeLightDark();
